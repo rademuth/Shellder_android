4c476940efcfd78e9709737ef0f7db04ef8a2c11
rademuth <rad9aw@virginia.edu>
2015-09-23 12:01:39 -0400

    Added home screen activity with (static) progress bar

---
+++ app/src/main/java/edu/virginia/cs/cs4720/shellder/HomeActivity.java
@@ -0,0 +1,46 @@
+package edu.virginia.cs.cs4720.shellder;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        // Set up the progress bar
+    }
+
+    public void show_list(View v) {
+        // Switch to the bucket list activity
+        Intent intent = new Intent(getApplicationContext(), BucketListActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
